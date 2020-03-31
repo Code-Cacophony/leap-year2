c93f391604236679d58088c4e3c87895514d220e
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-03-31 13:40:33 -0700

    add divisible by 100 spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 var leapYear = function(year) {
-  if (year % 4 === 0) {
-  return true;
-} else {
+  if (year % 100 === 0) {
     return false;
-}
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 $(document).ready(function(){
